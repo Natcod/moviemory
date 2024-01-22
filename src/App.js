0569d77068d4f7e1c6b9c01c49d8039d22eec38a
Natcod <107844070+Natcod@users.noreply.github.com>
2024-01-22 14:36:00 +0300

    basic skeleton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,87 @@
-function App() {
-  return <div></div>;
-}
+import Logo from "./Logo";
+import Search from "./search";
+import Box from "./Box";
+import Movie from "./Movie";
+import NumResult from "./NumResult";
+import WatchedSummary from "./WatchedSummary";
 
-export default App;
+const movies = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
+const watched = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+
+export default function App() {
+  return (
+    <>
+      <NavBar>
+        <Logo />
+        <Search />
+        <NumResult />
+      </NavBar>
+      <Main>
+        <Box>
+          <MovieList movies={movies} />
+        </Box>
+        <Box>
+          <WatchedSummary watched={watched} />
+        </Box>
+      </Main>
+    </>
+  );
+}
+function NavBar({ children }) {
+  return <nav className="nav-bar">{children}</nav>;
+}
+function Main({ children }) {
+  return <main className="main">{children}</main>;
+}
+function MovieList({ movies }) {
+  return (
+    <ul className="list list-movies">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
---
+++ src/Logo.js
@@ -0,0 +1,9 @@
+import logo from "./img/logo.jpg";
+export default function Logo() {
+  return (
+    <div className="logo">
+      <img src={logo} alt="Logo"></img>
+      <h1>MovieMory</h1>
+    </div>
+  );
+}
