e5557906fb36cf1f7f714863389b5e70bda0c995
Natcod <107844070+Natcod@users.noreply.github.com>
2024-01-24 11:21:15 +0300

    taking security measures with https resquest

--- src/useMovie.js
+++ src/useMovie.js
@@ -14,7 +14,7 @@ export default function useMovie(query) {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query} `
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query} `
           );
           if (!res.ok)
             throw new Error("Something went wrong with fetching movies");
