6ba31c8c1c8d0a984bc19ea5b1093de682640a22
Natcod <107844070+Natcod@users.noreply.github.com>
2024-01-24 14:29:05 +0300

    localStorage added

--- src/App.js
+++ src/App.js
@@ -9,58 +9,13 @@ import { useState } from "react";
 import MovieDetails from "./MovieDetails";
 import useMovie from "./useMovie";
 import Loader from "./Loader";
-
-// const movies = [
-//   {
-//     imdbID: "tt1375666",
-//     Title: "Inception",
-//     Year: "2010",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-//   },
-//   {
-//     imdbID: "tt0133093",
-//     Title: "The Matrix",
-//     Year: "1999",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-//   },
-//   {
-//     imdbID: "tt6751668",
-//     Title: "Parasite",
-//     Year: "2019",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-//   },
-// ];
-// const watched = [
-//   {
-//     imdbID: "tt1375666",
-//     Title: "Inception",
-//     Year: "2010",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-//     runtime: 148,
-//     imdbRating: 8.8,
-//     userRating: 10,
-//   },
-//   {
-//     imdbID: "tt0088763",
-//     Title: "Back to the Future",
-//     Year: "1985",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-//     runtime: 116,
-//     imdbRating: 8.5,
-//     userRating: 9,
-//   },
-// ];
+import useLocalStorage from "./useLocalStorage";
 
 export default function App() {
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedId] = useState(null);
   const { movies, isLoading, error } = useMovie(query);
-  const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useLocalStorage([], "watched");
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -74,6 +29,7 @@ export default function App() {
 
   return (
     <>
+      {console.log(movies === "undefined")}
       <NavBar>
         <Logo />
         <NumResult movies={movies} />
@@ -81,13 +37,20 @@ export default function App() {
       </NavBar>
       <Main>
         <Box>
-          {isLoading && <Loader />}
-          {!isLoading && !error && (
-            <MovieList movies={movies} onSelectMovie={handleSelectMovie} />
+          {movies && movies.length === 0 ? (
+            <WelcomeMessage />
+          ) : (
+            <>
+              {isLoading && <Loader />}
+              {!isLoading && !error && (
+                <MovieList movies={movies} onSelectMovie={handleSelectMovie} />
+              )}
+              {error && <ErrorMessage message={error} />}
+            </>
           )}
-          {error && <ErrorMessage message={error} />}
         </Box>
-        <Box>
+
+        <Box movies={movies}>
           {selectedId ? (
             <MovieDetails
               movies={movies}
@@ -128,5 +91,12 @@ function ErrorMessage({ message }) {
       <span>🛑</span>
       {message}
     </p>
+  );
+}
+function WelcomeMessage() {
+  return (
+    <div className="welcome">
+      <p>Welcome to MovieMory. Search the desired movie!</p>
+    </div>
   );
 }
--- src/useMovie.js
+++ src/useMovie.js
@@ -6,31 +6,47 @@ export default function useMovie(query) {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
-
   useEffect(
     function () {
+      //   callback?.();
+      const controller = new AbortController();
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
+
           const res = await fetch(
-            `https://www.omdbapi.com/?apikey=${KEY}&s=${query} `
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query} `,
+            { signal: controller.signal }
           );
+
           if (!res.ok)
             throw new Error("Something went wrong with fetching movies");
 
           const data = await res.json();
-          if (data.Response === "false") throw new Error("Movie not found");
+          if (data.Response === "False") throw new Error("Movie not found");
+
           setMovies(data.Search);
           setError("");
         } catch (err) {
-          console.log(err.message);
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            console.log(err.message);
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
       }
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
       fetchMovies();
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
