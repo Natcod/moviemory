6a661c06f91c7e61a13169fd223a751980a9b6f7
Natcod <107844070+Natcod@users.noreply.github.com>
2024-02-14 16:35:38 +0300

    adding the ability to watch movies, but not working as intended

--- src/WatchedMovieList.js
+++ src/WatchedMovieList.js
@@ -1,9 +1,32 @@
+import { useEffect, useState, useCallback } from "react";
+
 export default function WatchedMovieList({ watched, onDeleteWatched }) {
+  const [title, setTitle] = useState("");
+
+  const redirectToMovie = useCallback(() => {
+    console.log(title);
+    window.location.href = `https://fmoviesz.to/filter?keyword=${title}`;
+  }, [title]);
+  function handleWatchNowClick(movieTitle) {
+    setTitle(movieTitle);
+  }
+  useEffect(() => {
+    if (title) {
+      redirectToMovie();
+    }
+  }, [title, redirectToMovie]);
   return (
     <ul className="list">
       {watched.map((movie) => (
-        <li key={watched.imdbID} >
-          <img src={movie.poster} alt={`${movie.title} poster`} />
+        <li key={movie.imdbID}>
+          <img
+            src={movie.poster}
+            alt={`${movie.title} poster`}
+            onClick={() => handleWatchNowClick(movie.title)}
+            // onClick={() =>
+            //   (window.location.href = `https://fmoviesz.to/filter?keyword=${movie.title}`)
+            // }
+          />
           <h3>{movie.title}</h3>
           <div>
             <p>
@@ -20,9 +43,17 @@ export default function WatchedMovieList({ watched, onDeleteWatched }) {
             </p>
             <button
               className="btn-delete"
-              onClick={()=>onDeleteWatched(movie.imdbID)}
+              onClick={() => onDeleteWatched(movie.imdbID)}
             >
               X
+            </button>
+          </div>
+          <div>
+            <button
+              className="watch--now"
+              onClick={() => handleWatchNowClick(movie.title)}
+            >
+              Watch now
             </button>
           </div>
         </li>
